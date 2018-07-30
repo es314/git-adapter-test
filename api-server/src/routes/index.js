4b249feae7b6e646590e207ce3d4803765c4ded1
LogicHerder <dev@logicherder.com>
2018-07-30 16:18:18 -0400

    Scaffolding via LogicHerder

--- api-server/src/routes/index.js
+++ api-server/src/routes/index.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     routers: [
-                    // REST CRUD API
-                ['/data-api', require('../../../rest-crud-api')],
-                        // Main Content
-                        ]
+        // REST CRUD API
+        ['/data-api', require('../../../rest-crud-api')],
+        // Main Content
+    ]
 }
--- api-server/src/server/configureServer/configureRoutes.js
+++ api-server/src/server/configureServer/configureRoutes.js
@@ -9,11 +9,11 @@ const { routers } = require('../../routes');
 function configureRoutes(app, config){
     info("configuring static routes");
 
-        let staticPath = "";
-                    staticPath = path.resolve(__dirname, '../../../../public');
+    let staticPath = "";
+    staticPath = path.resolve(__dirname, '../../../../public');
     info(`configuring / to serve from: ${staticPath}`);
     app.use('/', express.static(staticPath));
-            
+
 
     routers.forEach(([ baseRoute, configureRoutes ]) => {
         info(`configuring routes for ${baseRoute}`);
