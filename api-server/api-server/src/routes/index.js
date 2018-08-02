5eef301c97c775a2af8feb08a055c0e5fbb73f62
LogicHerder <dev@logicherder.com>
2018-08-02 10:28:47 -0400

    Scaffolding via LogicHerder

---
+++ api-server/api-server/src/routes/index.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    routers: [
+        // Main Content
+    ]
+}
---
+++ api-server/api-server/src/server/configureServer/configureRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const logger = require('../../logger');
+
+const { warn, info, err } = logger.createChannel("config-routes");
+
+const { routers } = require('../../routes');
+
+function configureRoutes(app, config){
+    info("configuring static routes");
+
+    let staticPath = "";
+    staticPath = path.resolve(__dirname, '../../../../public');
+    info(`configuring / to serve from: ${staticPath} relative to ${__dirname}`);
+    app.use('/', express.static(staticPath));
+
+
+    routers.forEach(([ baseRoute, configureRoutes ]) => {
+        info(`configuring routes for ${baseRoute}`);
+        let router = express.Router();
+        configureRoutes(router, app, config);
+        app.use(baseRoute, router);
+    });
+}
+
+module.exports = configureRoutes;
